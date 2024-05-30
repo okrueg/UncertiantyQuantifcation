879c477831a8bfbb292079088f863b8ec52953f0
okrueg <opkruege@asu.edu>
2024-05-30 18:57:59 +0200

    NN contour map
    Added contour map and ability to add points

--- data_set_generation.py
+++ data_set_generation.py
@@ -15,7 +15,7 @@ def exclusion_area(x: np.ndarray, y: np.ndarray, radius: int, position = np.ndar
 #--- Utilizes matplotlib to plot *Outdated* ---
 def plot_data_static(x: np.ndarray, y: np.ndarray, circle = None):
     figure, axes = plt.subplots()
-    axes.add_artist( circle )
+    #axes.add_artist( circle )
     axes.scatter(x[:,0], x[:,1],c=y)
     axes.set_ylim(-5,5)
     axes.set_xlim(-5,5)
@@ -23,13 +23,15 @@ def plot_data_static(x: np.ndarray, y: np.ndarray, circle = None):
 
 #-- Just a default tuned generation --#
 def generate_data():
-    x, y = make_classification(n_samples= 100,
-                                n_features=2,
-                                n_redundant=0,
-                                n_informative=2, 
-                                n_clusters_per_class=2,
-                                flip_y= 0,
-                                class_sep= 1.2,
-                                #random_state= rand
-                            )
+    # x, y = make_classification(n_samples= 100,
+    #                             n_features=2,
+    #                             n_redundant=0,
+    #                             n_informative=2, 
+    #                             n_clusters_per_class=1,
+    #                             flip_y= 0,
+    #                             class_sep= 1.2,
+    #                             #random_state= rand
+    #                         )
+    x,y = make_moons(2000,noise=0.1)
     return x,y
+    
